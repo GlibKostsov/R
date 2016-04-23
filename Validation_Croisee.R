5a38a704deb54825e06abe7bc9d406b374ee226f
GlibKostsov <glibkostsov@gmail.com>
2016-04-23 23:48:17 +0200

    Initial Commit

---
+++ Validation_Croisee.R
@@ -0,0 +1,61 @@
+
+######VALIDATION CROISEE
+##Regression Ridge
+
+
+head(dataTrees)<-trees
+
+n<-nrow(dataTrees)
+
+
+#Evaluation d'erreur en fonction de lambda
+
+EvaluationErreur<-function(lambda){
+  Stat<-0
+  for(v in 1:n){
+    
+    dataTrees<-trees[-v,]
+    X<-as.matrix(cbind(rep(1,n-1),dataTrees$Girth, dataTrees$Height))
+    Y<-dataTrees$Volume
+    A<-t(X)%*%X+(n-1)*lambda*diag(c(1,1,1))
+    B<-t(X)%*%Y
+    
+    theta<-solve(A,B)
+    test<-trees[v,]
+    
+    x<-as.matrix(cbind(1,test$Girth,test$Height))
+    Stat<-Stat+(test$Volume-x%*%theta)^2
+  }
+  return(Stat)
+}
+
+#Verifivation de la fonction
+
+EvaluationErreur(0.00005)
+EvaluationErreur(0.00006)
+EvaluationErreur(0.00007)
+
+
+#Plot - Erreurs en fonction de lambda
+
+lambda<-seq(0.00005,0.0004,by = 0.00001)
+T<-c()
+counter<-0
+
+for (i in lambda){
+  counter<-counter+1 
+  T[counter] <- EvaluationErreur(i)
+}
+
+plot(lambda,T)
+
+#Minimom de la fonction sur l'intervalle [0,1]
+
+MinimomLambda<-optimize(EvaluationErreur,lower= 0, upper = 1)$minimum
+MinimomEvaluationErreur<-optimize(EvaluationErreur,lower= 0, upper = 1)$objective
+
+#Ajoute la point mininom sur le plot
+points(MinimomLambda,MinimomEvaluationErreur, col = 'red',pch=20)
+
+
+getwd()
