dc9d77a5994a0ff84ea87cb4a114db99bb0c0bf1
GlibKostsov <glibkostsov@gmail.com>
2016-04-24 09:00:00 +0200

    Changing of gradient and a J fonction

---
+++ Regression_Ridge.R
@@ -0,0 +1,70 @@
+#####REGRESSION RIDGE
+
+install.packages("MASS")
+ibrary(MASS)
+
+
+head(trees)
+
+
+##Traitement des donnees 
+
+#Extraction du sample a etudier
+set.seed(111)
+indexTrees<-sample(31,25)
+dataTrees<-trees[indexTrees,]
+n<-nrow(dataTrees)
+lambda<-0.5
+
+X<-as.matrix(cbind(rep(1,n),dataTrees$Girth,dataTrees$Height))
+Volume<-as.matrix(dataTrees$Volume)
+
+A<-t(X)%*%X+n*lambda*diag(rep(1,3))
+b<-t(X)%*%Volume
+
+###Resolution exact d'une systeme lineaaire
+
+solve(A,b)
+
+
+##Fonction a minimiser
+Y<-Volume
+J(X,Volume)
+J<-function(X,Y,Theta){
+  n<-nrow(Y)
+  J<-1/(2*n)*t(X%*%Theta-Y)%*%(X%*%Theta-Y)+(lambda/2)*sum(Theta)^2
+  return(J)
+}
+
+
+###Descente de gradient 
+I<-as.matrix(rep(1,length(Theta)))
+pas<-0.001
+Theta<-as.matrix(c(rep(0,3)))
+
+#Stat<-as.data.frame(matrix(vector(), 0, 3, dimnames=list(c(), c("J","Err","Comp"))))
+
+Compteur<-0
+Erreurs<-c()
+Js<-c()
+
+while(Erreur>0.01){
+  Compteur<-Compteur+1
+  Jderiv<- (t(X)%*%X%*%Theta-t(X)%*%Volume + n*lambda*Theta
+  tempTheta<-Theta - pas*Jderiv
+  Erreur<-sum(Jderiv^2)
+  Erreurs[Compteur]<-Erreur
+  Js[Compteur]<-J(X,Volume,Theta)
+  Theta<-tempTheta
+  
+}
+
+
+
+
+
+
+
+
+#tepmTheta<-Theta*(I-n*lambda*pas) - pas*Jderiv
+
